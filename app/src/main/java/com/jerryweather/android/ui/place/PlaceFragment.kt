49eb1dd2c08c82bba7518324122ad001dcb07f57
ZhangJerry2715 <1443379107@qq.com>
2021-03-09 13:55:29 +0800

    切换城市和下拉刷新

--- app/src/main/java/com/jerryweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/jerryweather/android/ui/place/PlaceFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.jerryweather.android.MainActivity
 import com.jerryweather.android.R
 import com.jerryweather.android.ui.weather.WeatherActivity
 
@@ -37,7 +38,7 @@ class PlaceFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         bgImageView = activity!!.findViewById(R.id.bgImageView)
-        if (viewModel.isPlaceSaved()){
+        if (activity is MainActivity&&viewModel.isPlaceSaved()){
             val place=viewModel.getSavedPlace()
             val intent= Intent(context, WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
